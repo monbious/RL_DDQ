784a5923cc37eb452530a6faf38784834cafabce
monbious <1364143139@qq.com>
2022-09-28 15:00:53 +0800

    test

--- src/run.py
+++ src/run.py
@@ -468,7 +468,7 @@ def run_episodes(count, status):
     grounded_for_model = params['grounded']
     simulation_epoch_size = planning_steps + 1
 
-    if (agt == 6 or agt == 9) and params['trained_model_path'] == None and warm_start == 1:
+    if (agt == 6 or agt == 9 or agt == 10) and params['trained_model_path'] == None and warm_start == 1:
         print('warm_start starting ...')
         warm_start_simulation()
         print('warm_start finished, start RL training ...')
@@ -494,7 +494,7 @@ def run_episodes(count, status):
                 cumulative_turns += dialog_manager.state_tracker.turn_count
 
         # simulation
-        if (agt == 6 or agt == 9) and params['trained_model_path'] == None:
+        if (agt == 6 or agt == 9 or agt == 10) and params['trained_model_path'] == None:
             agent.predict_mode = True
             world_model.predict_mode = True
             # 用世界模型训练策略模型
@@ -553,7 +553,7 @@ def run_episodes(count, status):
     status['successes'] += successes
     status['count'] += count
 
-    if (agt == 6 or agt == 9) and params['trained_model_path'] == None:
+    if (agt == 6 or agt == 9 or agt == 10) and params['trained_model_path'] == None:
         save_model(params['write_model_dir'], agt, float(successes) / count, 
                    best_model['model'], best_res['epoch'], count)
         save_performance_records(params['write_model_dir'], agt, performance_records)
